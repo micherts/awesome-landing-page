cbf843c8e0b495adf42939aa5b0c176066f8199a
Michael Roberts <mjrobertsIT@gmail.com>
2022-09-28 23:11:01 +1000

    customised to HospoSure

---
+++ assets/js/contactus.js
@@ -0,0 +1,89 @@
+function submitToAPI(e) {
+  e.preventDefault();
+  var url = "https://6gcdonb7r7.execute-api.ap-southeast-2.amazonaws.com";
+
+  var mandatory = /[A-Za-z0-9\W]/;
+  if (!mandatory.test($("#form_name").val())) {
+    alert("Please enter your name");
+    return;
+  }
+
+  if ($("#form_email").val() == "") {
+    alert("Please enter your email");
+    return;
+  }
+
+  var emailRe = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
+  if (!emailRe.test($("#form_email").val())) {
+    alert("Please enter valid email address");
+    return;
+  }
+
+  // var mandatory = /[A-Za-z0-9\W]/;
+  // if (!mandatory.test($("#form_referral").val())) {
+  //   alert("Please advise how you found out about us?");
+  //   return;
+  // }
+
+  // if (!mandatory.test($("#form_owner").val())) {
+  //   alert("Please let us know if you currently own a cafe?");
+  //   return;
+  // }
+
+  // if (!mandatory.test($("#form_opening").val())) {
+  //   alert("Please let us know if you are planning to open a cafe?");
+  //   return;
+  // }
+
+  // if (!mandatory.test($("#form_experience").val())) {
+  //   alert(
+  //     "Please let us know how many years experience you have in hospitality management?"
+  //   );
+  //   return;
+  // }
+
+  // var name = $("#form_name").val();
+  // var email = $("#form_email").val();
+  // var telephone = $("#form_telephone").val();
+  // var referral = $("#form_referral").val();
+  // var owner = $("#form_owner").val();
+  // var opening = $("#form_opening").val();
+  // var experience = $("#form_experience").val();
+  // var financials = $("#form_financials").val();
+  // var costed = $("#form_costed").val();
+  // var costing = $("#form_costing").val();
+  // var message = $("#form_message").val();
+  var data = {
+    subject: "Contact Form",
+    name: $("#form_name").val(),
+    email: $("#form_email").val(),
+    // telephone: $("#form_telephone").val(),
+    // referral: $("#form_referral").val(),
+    // owner: $("#form_owner").val(),
+    // opening: $("#form_opening").val(),
+    // experience: $("#form_experience").val(),
+    // financials: $("#form_financials").val(),
+    // costed: $("#form_costed").val(),
+    // costing: $("#form_costing").val(),
+    message: $("#form_message").val(),
+  };
+
+  // console.log(JSON.stringify(data));
+
+  // send POST request
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      alert(
+        "Thank you for your interest in HospoSure, we look forward to chatting."
+      );
+      document.getElementById("contact-form").reset();
+      // console.log("Response: ", response);
+    })
+    .catch((error) => console.log("Error: ", error));
+}
---
+++ manage.ps1
@@ -0,0 +1,130 @@
+#Environment
+Set-AWSCredential -ProfileName AWSmicherts
+$Region = 'ap-southeast-2'
+$RegionS3R53ZoneID = "Z1WCIGYICN2BYD" #ap-southeast-2 S3 Bucket R53 Hosted Zone ID https://docs.aws.amazon.com/general/latest/gr/s3.html#s3_website_region_endpoints
+$App = 'hosposure.com.au' #All lowercase
+$WL = "$Home\Documents\awesome-landing-page"
+Set-Location $WL
+Import-Module AWS.Tools.Route53
+
+#Domain Set Up Process
+#Route53 - Register domain names.
+#S3 - Create bucket with public access
+ New-S3Bucket -Region $Region -BucketName $App -CannedACLName public-read
+ Write-S3BucketWebsite -BucketName $App -WebsiteConfiguration_IndexDocumentSuffix index.html -WebsiteConfiguration_ErrorDocument error.html
+ $Policy = [PSCustomObject]@{Version = "2012-10-17"; Statement = [PSCustomObject]@{Sid = "PublicReadGetObject"; Effect = "Allow"; Principal = "*"; Action = @("s3:GetObject"); Resource = @("arn:aws:s3:::$App/*")}}
+ Write-S3BucketPolicy -BucketName $App -Policy ($Policy | ConvertTo-Json)
+#S3 - Copy local files to S3
+ $Files = Get-ChildItem | Where-Object {$_.Name -ne 'manage.ps1'}
+ #$Files = Get-ChildItem | Where-Object {$_.Name -eq 'index.html'}
+ ForEach ($File in $Files) {
+  $FileName = [System.IO.Path]::GetFileName($File)
+  If ($File.Mode -match "d") {
+    $Prefix = "$($File.FullName.ToString().replace($WL,''))\"
+    Write-S3Object -BucketName $App -Folder $File.FullName -KeyPrefix $Prefix -Recurse -Verbose -Force 
+  } else {
+  Write-S3Object -BucketName $App -Key $FileName -File $FileName -Verbose -Force 
+  }
+ }
+#Certificate Manager - Create new certificate for domain and *.domain.
+#Certificate Manager - Validate domains by 'Create records in Route 53' option.
+#CloudFront - Create new distribution for S3 bucket with alternate domain names domain and www.domain, default root object index.html, behaviour redirect http to https.
+#Route53 - Add hosted zone A record to route to CloudFront distribution.
+ $R53HostedZoneId = (Get-R53HostedZones | Where {$_.Name -eq "$App."}).Id.split('/')[2]
+ $Record = New-Object Amazon.Route53.Model.Change
+ $Record.Action = "CREATE"
+ $Record.ResourceRecordSet = New-Object Amazon.Route53.Model.ResourceRecordSet
+ $Record.ResourceRecordSet.Name = "$App."
+ $Record.ResourceRecordSet.Type = "A"
+ <# These lines need updating
+ $Record.ResourceRecordSet.AliasTarget = New-Object Amazon.Route53.Model.AliasTarget
+ $Record.ResourceRecordSet.AliasTarget.HostedZoneId = $RegionS3R53ZoneID
+ $Record.ResourceRecordSet.AliasTarget.DNSName = "s3-website-$Region.amazonaws.com."
+ $Record.ResourceRecordSet.AliasTarget.EvaluateTargetHealth = $true #>
+ Edit-R53ResourceRecordSet -HostedZoneId $R53HostedZoneId -ChangeBatch_Comment "Add routing to CloudFront" -ChangeBatch_Change $Record
+#Route53 - Add hosted zone CNAME record to route www subdomain to apex.
+ $Record = New-Object Amazon.Route53.Model.Change
+ $Record.Action = "CREATE"
+ $Record.ResourceRecordSet = New-Object Amazon.Route53.Model.ResourceRecordSet
+ $Record.ResourceRecordSet.Name = "www.$App."
+ $Record.ResourceRecordSet.Type = "CNAME"
+ $Record.ResourceRecordSet.ResourceRecords = (New-Object Amazon.Route53.Model.ResourceRecord)
+ $Record.ResourceRecordSet.ResourceRecords[0].Value = "$App."
+ $Record.ResourceRecordSet.TTL = 500
+ Edit-R53ResourceRecordSet -HostedZoneId $R53HostedZoneId -ChangeBatch_Comment "Add routing from www subdomain to apex" -ChangeBatch_Change $Record
+#Route secondary domain to main domain. Only works for http, not https. Secondary domain https routing is not possible on AWS.
+ $App2 = 'hosposure.com'
+ $R53HostedZoneId2 = (Get-R53HostedZones | Where {$_.Name -eq "$App2."}).Id.split('/')[2]
+ #S3 - Create bucket for redirect
+  New-S3Bucket -Region $Region -BucketName $App2
+  Write-S3BucketWebsite -BucketName $App2 -RedirectAllRequestsTo_HostName $App
+ #Route53 - Add hosted zone A record to route to S3.
+  $Record = New-Object Amazon.Route53.Model.Change
+  $Record.Action = "CREATE"
+  $Record.ResourceRecordSet = New-Object Amazon.Route53.Model.ResourceRecordSet
+  $Record.ResourceRecordSet.Name = "$App2."
+  $Record.ResourceRecordSet.Type = "A"
+  $Record.ResourceRecordSet.AliasTarget = New-Object Amazon.Route53.Model.AliasTarget
+  $Record.ResourceRecordSet.AliasTarget.HostedZoneId = $RegionS3R53ZoneID
+  $Record.ResourceRecordSet.AliasTarget.DNSName = "s3-website-$Region.amazonaws.com."
+  $Record.ResourceRecordSet.AliasTarget.EvaluateTargetHealth = $false
+  Edit-R53ResourceRecordSet -HostedZoneId $R53HostedZoneId2 -ChangeBatch_Comment "Route secondary domain to main domain" -ChangeBatch_Change $Record
+ #Create redirects for subdomains
+  ForEach ($Sub in ("app", "www")) {
+   #S3 - Create bucket for redirect
+    New-S3Bucket -Region $Region -BucketName "$Sub.$App2"
+    Write-S3BucketWebsite -BucketName "$Sub.$App2" -RedirectAllRequestsTo_HostName "$Sub.$App"
+   #Route53 - Add hosted zone CNAME record to route subdomain to apex.
+    $Record = New-Object Amazon.Route53.Model.Change
+    $Record.Action = "CREATE"
+    $Record.ResourceRecordSet = New-Object Amazon.Route53.Model.ResourceRecordSet
+    $Record.ResourceRecordSet.Name = "$Sub.$App2."
+    $Record.ResourceRecordSet.Type = "CNAME"
+    $Record.ResourceRecordSet.ResourceRecords = (New-Object Amazon.Route53.Model.ResourceRecord)
+    $Record.ResourceRecordSet.ResourceRecords[0].Value = "$App2."
+    $Record.ResourceRecordSet.TTL = 500
+    Edit-R53ResourceRecordSet -HostedZoneId $R53HostedZoneId2 -ChangeBatch_Comment "Add routing from $Sub subdomain to apex" -ChangeBatch_Change $Record
+   }
+
+
+#Delete S3
+#Get-S3Object -BucketName $App | Remove-S3Object -Verbose -Force
+#Remove-S3Bucket -Region $Region -BucketName $App -Force -Verbose
+
+#Read S3
+Get-S3Object -Region $Region -BucketName $App | ft
+
+#Copy S3 files to local
+$Files = Get-S3Object -Region $Region -BucketName $App
+ForEach ($File in $Files) {
+    Read-S3Object -BucketName $App -Key $File.Key -File $File.Key -Verbose
+}
+
+#Read R53 Record Sets
+$R53HostedZoneId = (Get-R53HostedZones | Where {$_.Name -eq "$App."}).Id.split('/')[2]
+$Records = (Get-R53ResourceRecordSet -HostedZoneId $R53HostedZoneId).ResourceRecordSets
+$Records | ft
+
+#Create R53 Record to route apex domain to S3 bucket
+
+
+
+
+
+
+# Initialise git
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+# Manually create new repo on Github
+git remote add origin git@github.com:micherts/hosposure.com.au.git
+git push -u origin main
+
+# Update git **Note git used for backup only, S3 used for primary storage
+git add .
+git commit -m "updated contactus.js to include subject"
+git push origin main
+
+#Clone Git repo
+git clone git@github.com:micherts/awesome-landing-page.git
