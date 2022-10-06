9b297e1849e95597297ba5d816661115c0f4622a
Michael Roberts <mjrobertsIT@gmail.com>
2022-10-06 16:53:19 +1100

    updated layout

--- manage.ps1
+++ manage.ps1
@@ -122,7 +122,7 @@ git push -u origin main
 
 # Update git **Note git used for backup only, S3 used for primary storage
 git add .
-git commit -m "removed company logos and updated text"
+git commit -m "updated layout"
 git push origin master
 
 #Clone Git repo
