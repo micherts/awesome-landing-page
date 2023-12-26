76e48ae98be1c1eaf785ba477cfa25fdcdac4ae7
Michael Roberts <michael.roberts@hosposure.com.au>
2023-12-26 15:27:08 +1100

    created resources pages, re-factored css headings and paragraphs

--- manage.ps1
+++ manage.ps1
@@ -129,7 +129,7 @@ Start-Process "C:\Program Files\Google\Chrome\Application\chrome.exe" "http://lo
 
 # Update git **Note git workflow syncs to S3
 git add .
-git commit -m "created faq page"
+git commit -m "created resources pages, re-factored css headings and paragraphs"
 git push origin master
 
 #Clone Git repo
