3110da2882649b6861fc9232ec7d157b2220c7a8
Michael Roberts <michael.roberts@hosposure.com.au>
2024-01-04 20:39:57 +1100

    expanded resources page, duplicated nav and footer across all 4 pages instead of using js, tweaked css, removed local copies of bootstrap css and fonts

--- assets/js/common.js
+++
@@ -1,6 +0,0 @@
-// https://www.geeksforgeeks.org/how-to-load-external-html-file-using-jquery
-
-https: $(document).ready(function () {
-  $(document).load("nav.html");
-  $(document).load("footer.html");
-});
