4b15df7358db404820a30a5467e22c61e377b15e
Michael Roberts <michael.roberts@hosposure.com.au>
2024-03-29 23:14:28 +1100

    updated css for navbar btn

--- manage.ps1
+++ manage.ps1
@@ -129,7 +129,7 @@ Start-Process "C:\Program Files\Google\Chrome\Application\chrome.exe" "http://lo
 
 # Update git **Note git workflow syncs to S3
 git add .
-git commit -m "added email welcome image"
+git commit -m "updated css for navbar btn"
 git push origin master
 
 #Clone Git repo
