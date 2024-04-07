dc9b5f9928fc57c38e816789a3e11e5b47ca74ee
Michael Roberts <michael.roberts@hosposure.com.au>
2024-04-07 13:18:25 +1000

    added mail icon to assets

--- manage.ps1
+++ manage.ps1
@@ -129,7 +129,7 @@ Start-Process "C:\Program Files\Google\Chrome\Application\chrome.exe" "http://lo
 
 # Update git **Note git workflow syncs to S3
 git add .
-git commit -m "updated css for navbar btn"
+git commit -m "added mail icon to assets"
 git push origin master
 
 #Clone Git repo
